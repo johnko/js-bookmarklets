24e2f357c680ca7e6cfa33ad2bd3bf2fd5f90506
John Ko <git@johnko.ca>
2016-01-15 02:28:16 +0000

    Update i2p.scrape.magnet.links.js

--- i2p.scrape.magnet.links.js
+++ i2p.scrape.magnet.links.js
@@ -2,6 +2,7 @@
 This example searches the current page for <a> elements that contain magnet links and posts the magnet link to i2psnark
 */
 function doit(){
+    var nonce="123";
     var torrentclientapi="http://localhost:7657/i2psnark/_post";
     var a=document.getElementsByTagName("a");
     for(var i=0;i<a.length;i++){
@@ -10,7 +11,7 @@ function doit(){
             var xhr=new XMLHttpRequest();
             xhr.open("POST",torrentclientapi,true);
             xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-            xhr.send("action=Add&foo=Add+torrent&nofilter_newDir=&nofilter_newURL="+encodeURIComponent(magnet));
+            xhr.send("nonce="+nonce+"&action=Add&foo=Add+torrent&nofilter_newDir=&nofilter_newURL="+encodeURIComponent(magnet));
         }
     }
 }
