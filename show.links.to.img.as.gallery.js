99b807d2becad1a51153df338f6bfb7773fd1f01
John Ko <git@johnko.ca>
2016-01-15 01:55:50 +0000

    Create show.links.to.img.as.gallery.js

---
+++ show.links.to.img.as.gallery.js
@@ -0,0 +1,18 @@
+function doit(){
+    var a=document.getElementsByTagName("a");
+    var d=document.createElement("div");
+    var z=[];
+    for(var i=0;i<a.length;i++){
+        var imgurl=a[i].href;
+        if((imgurl.match(/(png|jpg|gif)$/))&&(z.indexOf(imgurl)<0)){
+            var img=document.createElement("img");
+            img.src=imgurl;
+            img.style="border:4px solid #444;max-width:100%;max-height:800px;";
+            d.appendChild(img);
+            z.push(imgurl);
+        }
+    }
+    d.style="background:#111;text-align:center;position:absolute;top:0px;max-width:100%;";
+    document.getElementsByTagName("body")[0].appendChild(d);
+}
+doit();
